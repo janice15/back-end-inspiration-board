33ebcb13901c0c9b59e860ed55ac1344834a819d
Anna Lima <annavanwesep@gmail.com>
2023-06-27 10:56:44 -0700

    added to and from_dict helper functions to app

--- app/models/card.py
+++ app/models/card.py
@@ -3,4 +3,18 @@ class Card(db.Model):
     card_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     message = db.Column(db.String(80))
     likes_count = db.Column(db.Integer)
-    
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "message": self.message,
+            "likes_count": self.likes_count
+        }
+
+    @classmethod
+    def from_dict(cls, card_details):
+        new_message = cls(
+            message=card_details["message"],
+            likes_count=card_details["likes_count"]
+        )
+        return new_message
