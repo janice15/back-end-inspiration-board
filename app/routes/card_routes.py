9b566397952aeb3a27a5efb3a69b663029aec7f6
Susi <susi.franco32@gmail.com>
2023-06-27 10:48:52 -0700

    created patch route for card likes_count

--- app/routes/card_routes.py
+++ app/routes/card_routes.py
@@ -56,3 +56,12 @@ def delete_one_card(card_id):
 
     return f"Card {card_to_delete.name} is deleted!", 200
 
+# patch route for likes count. 
+@cards_bp.route("/<card_id>", methods=["PATCH"])
+def patch_one_card(card_id):
+    card_to_update = get_valid_item_by_id(Card, card_id)
+    
+    # update card likes count to increment by 1.
+    card_to_update.likes_count += 1
+    db.session.commit()
+    return card_to_update.to_dict(), 200
